4c92af4e02bbd20405385a43b41d23fb5c715a21
unknown <ebubeidika@gmail.com>
2024-07-11 12:35:51 +0100

    model modificatiopn

--- authentication/models.py
+++ authentication/models.py
@@ -30,7 +30,8 @@ class AccountProfile(models.Model):
         ('CHK', 'Checking'),
     ]
     account_type = models.CharField(max_length=3, choices=ACCOUNT_TYPES)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(max_digits=13, decimal_places=2)
+    pending_balance=models.DecimalField(max_digits=13, decimal_places=2,null=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.account_type}"
