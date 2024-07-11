bddc4283f9b086e17a44042377cecce176782a56
unknown <ebubeidika@gmail.com>
2024-07-11 12:53:47 +0100

    model modificatiopn

--- authentication/views.py
+++ authentication/views.py
@@ -19,7 +19,7 @@ def login(request):
     user=get_object_or_404(User,username=request.data['username'])
     #Checking if the users password matches 
     if not user.check_password(request.data['password']):
-        return Response({"details":"Info Not Found"})
+        return Response({"details":"Wrong Password"})
     
     # Getting the users token or generating one if it dosnt exist
     token,created=Token.objects.get_or_create(user=user)
