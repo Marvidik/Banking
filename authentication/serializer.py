164ef721e73548f622122cf954fb6d9db3f56a44
unknown <ebubeidika@gmail.com>
2024-07-26 21:40:21 +0100

    serialize id

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -39,7 +39,7 @@ class AccountProfileSerializer(serializers.ModelSerializer):
 class MoneyTransferSerializer(serializers.ModelSerializer):
     class Meta:
         model = MoneyTransfer
-        fields = ['user', 'recipient_name', 'recipient_account_number', 'recipient_routing_number', 'recipient_bank_name', 'amount','status_type','date','transaction_type']
+        fields = ['id','user', 'recipient_name', 'recipient_account_number', 'recipient_routing_number', 'recipient_bank_name', 'amount','status_type','date','transaction_type']
 
 
 
